97bf472d931c4b08509eca89f80d5225a6eb8f3c
Sankalpa Chowdhury <38287341+sankalpachowdhury@users.noreply.github.com>
2020-04-17 20:58:12 +0530

    Update sketch.js

--- part2/Load_model/sketch.js
+++ part2/Load_model/sketch.js
@@ -1,14 +1,12 @@
 // ml5.js: Pose Classification
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/Courses/ml5-beginners-guide/7.2-pose-classification.html
-// https://youtu.be/FYgYyq-xqAw
 
-// All code: https://editor.p5js.org/codingtrain/sketches/JoZl-QRPK
+
+// All code: 
 
 // Separated into three sketches
-// 1: Data Collection: https://editor.p5js.org/codingtrain/sketches/kTM0Gm-1q
-// 2: Model Training: https://editor.p5js.org/codingtrain/sketches/-Ywq20rM9
-// 3: Model Deployment: https://editor.p5js.org/codingtrain/sketches/c5sDNr8eM
+// 1: Data Collection: 
+// 2: Model Training: 
+// 3: Model Deployment: 
 
 let video;
 let poseNet;
